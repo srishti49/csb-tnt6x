98165135502fc014d949dc8b564db2d1c4777f3c
Code Sandbox <hello@codesandbox.io>
2021-06-12 18:27:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.62f4b49dfc7f608d70a82f33f2d1324f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f128d1443f9ea78774bed7c30d8b220e",
+    "url": "https://srishti49.github.io/csb-tnt6x/index.html"
+  },
+  {
+    "revision": "8b4fa695a1386f27cf0a",
+    "url": "https://srishti49.github.io/csb-tnt6x/static/js/2.d49db57b.chunk.js"
+  },
+  {
+    "revision": "75a932d05a223a76edd0",
+    "url": "https://srishti49.github.io/csb-tnt6x/static/js/main.03bc825f.chunk.js"
+  },
+  {
+    "revision": "a2d4626db75b305c3cb2",
+    "url": "https://srishti49.github.io/csb-tnt6x/static/js/runtime-main.afae50d4.js"
+  }
+]);
